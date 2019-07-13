3db0dd533fdc607b4537d5fbf12db1ec651c9219
Jason Bourne <m.bourne@wi.rr.com>
2019-07-13 12:34:32 -0500

    removed port

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    // port: 3306,
     user: "root",
     password: "password",
     database: "burger_db"
@@ -13,7 +13,7 @@ var connection = mysql.createConnection({
     if (err){
       console.log("Error: " + err)
 
-      throw err;
+      // throw err;
     } 
     console.log("connected as id " + connection.threadId);
   });
